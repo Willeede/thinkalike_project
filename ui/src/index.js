ba35083a4e0cc23a620750313f52693bc62a0352
Eede <55264075+Willeede@users.noreply.github.com>
2025-02-09 17:11:19 -0300

    Update index.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import DataTraceability from './components/DataTraceability'; // Import DataTraceability Component - UI Validation Revolution MASTERPIECE!
-import dataFlow from './data/DataTraceabilityExampleData'; // Import Example Data - Fueling the Showcase!
-import { CssBaseline, Container, Typography, Box } from '@mui/material'; // Material UI - For Pixel-Perfect UI
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/globalStyles.css'; // Global styles - ZENITH OF VISUAL HARMONY!
 
-function App() {
-  return (
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
 
-        <CssBaseline /> 
 
-        <Container maxWidth="md"> 
-          
-            
-              UI Validation Revolution: DataTraceability ETHICAL DATA VALIDATION SHOWCASE - ZENITH OF EXCELLENCE EDITION
-            
-            
 
-            <DataTraceability dataFlow={dataFlow} /> { /* DataTraceability Component Instance - UNLEASH THE MASTERPIECE! */ }
+            <App />
 
-            
-              ThinkAlike Project - UI Validation Revolution - Ethical Data Dream Coded into Reality! - MASTERPIECE UNVEILED!
-            
-          
-        </Container>
 
-  );
-}
 
-export default App;
+);
