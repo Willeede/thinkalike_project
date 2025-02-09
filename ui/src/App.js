79d38252deee7e217dd6976d494ef95c868fdbd5
Eede <55264075+Willeede@users.noreply.github.com>
2025-02-09 17:12:30 -0300

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import DataTraceability from './components/DataTraceability'; // Import DataTraceability Component - UI Validation Revolution MASTERPIECE!
+import dataFlow from './data/DataTraceabilityExampleData'; // Import Example Data - Fueling the Showcase!
+import { CssBaseline, Container, Typography, Box } from '@mui/material'; // Material UI - For Pixel-Perfect UI
 
+function App() {
+  return (
+
+        <CssBaseline /> 
+
+        <Container maxWidth="md"> 
+          
+            
+              UI Validation Revolution: DataTraceability ETHICAL DATA VALIDATION SHOWCASE - ZENITH OF EXCELLENCE EDITION
+            
+            
+
+            <DataTraceability dataFlow={dataFlow} /> { /* DataTraceability Component Instance - UNLEASH THE MASTERPIECE! */ }
+
+            
+              ThinkAlike Project - UI Validation Revolution - Ethical Data Dream Coded into Reality! - MASTERPIECE UNVEILED!
+            
+          
+        </Container>
+
+  );
+}
+
+export default App;
