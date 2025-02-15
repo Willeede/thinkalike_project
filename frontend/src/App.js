a5a638b83973da6f0de1394fd9c853dc53ca72ae
Eede <55264075+Willeede@users.noreply.github.com>
2025-02-15 16:48:35 -0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import DataTraceability from './components/DataTraceability'; // Import DataTraceability component
+import dataFlow from './data/DataTraceabilityExampleData'; // Import example DataFlow data
+
+function App() {
+  return (
+    
+
+        
+            UI Validation Revolution - DataTraceability ETHICAL DATA VALIDATION SHOWCASE
+        
+        
+            {/* DataTraceability Component Instance - ETHICAL DATA VALIDATION SHOWCASE - ZENITH OF EXCELLENCE EDITION! */}
+            <DataTraceability dataFlow={dataFlow} />
+        
+    
+
+  );
+}
+
+export default App;
