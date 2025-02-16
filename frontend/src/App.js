6d425b36444f8e747721d6b00a8cee0969373c2b
Eede <55264075+Willeede@users.noreply.github.com>
2025-02-16 16:00:17 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
+import React from 'react';
+import DataTraceability from './components/DataTraceability'; // Import DataTraceability component
+import dataFlow from './data/DataTraceabilityExampleData'; // Import example DataFlow data
+
 function App() {
   return (
-    <div className="App">  {/* <--- Add enclosing <div> tag here */}
+    <div className="App">
       <header className="App-header">
         
-            UI Validation Revolution - DataTraceability ETHICAL DATA VALIDATION SHOWCASE
+          UI Validation Revolution - DataTraceability ETHICAL DATA VALIDATION SHOWCASE
         
       </header>
       <section className="content">
         {/* DataTraceability Component Instance - ETHICAL DATA VALIDATION SHOWCASE - ZENITH OF EXCELLENCE EDITION! */}
         <DataTraceability dataFlow={dataFlow} />
       </section>
-    </div>         {/* <--- Add closing </div> tag here */}
+    </div>
   );
 }
+
+export default App;
