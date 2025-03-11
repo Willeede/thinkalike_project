6634f175bee782b533ea782778c98d0aedf4616b
Eede <55264075+Willeede@users.noreply.github.com>
2025-03-11 16:28:40 -0300

    update fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Get the root element
+const container = document.getElementById('root');
+
+// Create a root
+const root = createRoot(container);
+
+// Render the app
+root.render(<App />);
